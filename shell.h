2e6837b565699f6559438884521e97db655f1122
abdellatif nadir <nadirabdellatif50@gmail.com>
2023-08-18 14:16:54 -0700

    task 0 with shell header

---
+++ shell.h
@@ -0,0 +1,13 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+/* library */
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+
+
+
+#endif /* end of shell_h*/
