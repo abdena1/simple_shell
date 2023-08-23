f7990da898254a7611db76d82b266b1497944856
abdellatif nadir <nadirabdellatif50@gmail.com>
2023-08-23 03:04:04 -0700

    update

--- shell.h
+++ shell.h
@@ -1,16 +1,34 @@
 #ifndef SHELL_H
 #define SHELL_H
+#define _GNU_SOURCE
 
-/* library */
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <stdlib.h>
-#include <sys/wait.h>
-void promt(char **av, char **env);
-
 #include <sys/types.h>
-#define PATH_MAX_LENGTH 1024
-#define MAX_COMMAND 10
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <errno.h>
 
-#endif /* end of shell_h*/
+extern char **environ;
+
+int _strcmp(char *s1, char *s2);
+size_t _strncmp(char *s1, char *s2, size_t n);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+char *_strcat(char *dest, char *src);
+int _putchar(char c);
+
+char *_get_path(char **env);
+int _values_path(char **arg, char **env);
+char *_getline_command(void);
+void _getenv(char **env);
+char **_get_token(char *lineptr);
+void _exit_command(char **args, char *lineptr, int _exit);
+int _fork_fun(char **arg, char **av, char **env,
+char *lineptr, int np, int c);
+char *_strtok(char *str, const char *delim);
+
+
+#endif /* SHELL_H */
