078e2fdb72d54835e231165e4f027c9980db4b44
Your Name <you@example.com>
2023-08-23 13:55:20 -0700

    bug fix

--- read_line.c
+++ read_line.c
@@ -1,9 +1,13 @@
 #include "shell.h"
 /**
- * *read_line - get iput of user as a string
- * Return: user input of a string
+ * read_line - Read a line of user input from stdin.
+ * The fction rd a ln of txt entered by user from the stdrd input.
+ * uses dynamic mem allocation to store the input str and handles cases
+ * where memory allocation or rd fails. The returned str should be freed
+ * when it is no longer needed to prevent memory leaks.
+ * Return: A dynamically allocated string containing the user's input.
+ * Returns NULL on failure or upon reaching end-of-file.
  */
-
 char *read_line(void)
 {
 	size_t bufer = 0;
